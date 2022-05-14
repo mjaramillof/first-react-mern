bb2383e44635c260a26acfd3e8a4453c4c5e287e
Mateo <mateojaramillof@gmail.com>
2022-05-14 11:21:39 -0500

    [New Change]

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><span><PersonCard lastName = {"Doe"}/>, <PersonCard firstName = {"Jane"}/></span> </h1>
+      <span>Age: <PersonCard age={45}/></span>
+      <span>Hair Color: <PersonCard hairColor = {"Black"}/></span>
+      <h1><span><PersonCard lastName = {"Smith"}/>, <PersonCard firstName = {"John"}/></span> </h1>
+      <span>Age: <PersonCard age={88}/></span>
+      <span>Hair Color: <PersonCard hairColor = {"Brown"}/></span>
+      <h1><span><PersonCard lastName = {"Fillmore"}/>, <PersonCard firstName = {"Millard"}/></span> </h1>
+      <span>Age: <PersonCard age={50}/></span>
+      <span>Hair Color: <PersonCard hairColor = {"Brown"}/></span>
+      <h1><span><PersonCard lastName = {"Smith"}/>, <PersonCard firstName = {"Maria"}/></span> </h1>
+      <span>Age: <PersonCard age={45}/></span>
+      <span>Hair Color: <PersonCard hairColor = {"Brown"}/></span>
     </div>
   ); 
 }
